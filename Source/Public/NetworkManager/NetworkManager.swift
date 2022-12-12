df17203c6eb61b43100c1d09847c9bae5be6ceb1
Amit Garg <09s.amitgarg@gmail.com>
2022-12-12 16:09:16 +0530

    Add shared file

---
+++ Source/Public/NetworkManager/NetworkManager.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.swift
+//  AKNetworkManager
+//
+//  Created by Amit Garg on 12/12/22.
+//
+
+import Foundation
+
+public class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    public func execute() {
+        print("Execution")
+    }
+}
