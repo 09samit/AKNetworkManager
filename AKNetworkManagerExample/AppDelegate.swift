5ec90cb73ffcd10ffd179d29c7f94e717af9fcd1
Amit Garg <09s.amitgarg@gmail.com>
2022-12-16 12:14:47 +0530

    Minor change

--- AKNetworkManagerExample/AppDelegate.swift
+++ AKNetworkManagerExample/AppDelegate.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import AKNetworkManager
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -14,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        NetworkManager.shared.setup(url: "https://shmchat.com/api/", key: "#%2s$h#1m2c$%ha^t5", version: "1")
+        
         return true
     }
 
--- AKNetworkManagerExample/ViewController.swift
+++ AKNetworkManagerExample/ViewController.swift
@@ -6,10 +6,62 @@
 //
 
 import UIKit
+import Alamofire
 import AKNetworkManager
 class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        getAppSetting { _,_ in
+           
+        }
     }
 }
 
+struct AppSetting: Codable {
+  
+    var iOSVersion: String = ""
+    var isiOSVersionForceToUpdate : Int = 0
+    var androidVersion: String = ""
+    var isAndroidVersionForceToUpdate : Int = 0
+    var isAdsEnabled: Int = 1
+    var adURL: String = "http://shmchat.com1111"
+
+    enum CodingKeys: String, CodingKey {
+        case iOSVersion = "ios_version"
+        case isiOSVersionForceToUpdate = "ios_version_force_update"
+        case androidVersion = "android_version"
+        case isAndroidVersionForceToUpdate = "android_version_force_update"
+        case isAdsEnabled = "ad_enable"
+        case adURL = "ad_url"
+
+    }
+    
+    var isForcedUpdateNeedToTrigger: Bool {
+        if isiOSVersionForceToUpdate == 0 {
+            return false
+        }
+        let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as! String
+        let versionCompare = version.compare(self.iOSVersion, options: .numeric)
+        if versionCompare == .orderedAscending {
+            return true
+        }
+        return false
+    }
+}
+
+extension ViewController {
+    func getAppSetting(block: @escaping (AppSetting?, DataError?)->Void) {
+        let _ = NetworkManager.shared.request("appSetting", method: .get, isAuthorizationRequired: false, encoding: URLEncoding.default) { (result : DataResult<AppSetting>) in
+            switch result {
+            case .Success(let object):
+                if let data = object.data {
+                    block(data, nil)
+                }
+                break
+            case .Failure(let error):
+                 block(nil, error)
+                break
+            }
+        }
+    }
+}
